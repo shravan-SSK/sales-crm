f743cf28a0634a68f6c936e9f0f876160cdbac6d
shravan-SSK <shravan@seventhtriangle.com>
2026-03-26 09:49:54 +0530

    Add frontend/src/App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Layout from './components/Layout'
+import Dashboard from './pages/Dashboard'
+import Leads from './pages/Leads'
+import Contacts from './pages/Contacts'
+import Accounts from './pages/Accounts'
+import Pipeline from './pages/Pipeline'
+import Activities from './pages/Activities'
+import Settings from './pages/Settings'
+import EmailThreads from './pages/EmailThreads'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="leads" element={<Leads />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="accounts" element={<Accounts />} />
+          <Route path="pipeline" element={<Pipeline />} />
+          <Route path="activities" element={<Activities />} />
+          <Route path="emails" element={<EmailThreads />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
